57cb4bbb4da4a3460b59f3ac49a09e8911ce5e75
stakhur <pushthebo@gmail.com>
2024-10-13 12:38:08 +0200

    start working on GTNRules

--- seabattle/rules.py
+++ seabattle/rules.py
@@ -1,7 +1,47 @@
 class Rules:
 
-    def __init__(self, min=0, max=10):
+    def __init__(self, limits: dict):
+        self._limits = limits
+        self._LIMITS = tuple(limits.keys())
+
+
+    @property
+    def LIMITS(self):
+        return self._LIMITS
+
+
+    @property
+    def limits(self):
+        return self._limits
+    
+
+    def set_limit(self, name, value):
+        self._limits[name] = value
+
+
+    def make_preparations(self):
         pass
 
-    def get_available_positions(self):
+
+    def make_turn(self):
         pass
+
+class GuessTheNumberRules(Rules):
+    
+    def __init__(self):
+        super.__init__(limits = {
+            "min": 0,
+            "max": 10,
+            "players": 2,
+        })
+
+        self._target = None
+
+    def make_preparations(self):
+        input_str = f"Enter the target between {self.limits["min"]} and {self.limits["max"]}: "
+        target = input(input_str)
+        while ((not target.isdigit()) or
+               (int(target) < self.limits["min"] or int(target) > self.limits["max"])):
+            target = input(input_str)
+
+        self._target = int(target)
--- tests/test_rules.py
+++ tests/test_rules.py
@@ -2,13 +2,54 @@ import pytest
 
 from seabattle.rules import Rules
 
-def test_rule():
-    rule = Rules()
 
-def test_set_limits():
-    rule = Rules(0, 10)
+@pytest.fixture
+def rule():
+    _rule = Rules({})
+    return _rule
 
 
-def test_get_available_field():
-    rule_gtn = Rules()
-    rule_gtn.get_available_positions()
+def test_rule(rule):
+    assert isinstance(rule, Rules)
+    assert rule._limits == dict()
+
+
+def test_set_limits(rule):
+    limit_min = "min"
+    value = 1
+    rule.set_limit(limit_min, value)
+    assert limit_min in rule.limits
+    assert rule.limits[limit_min] == value
+
+    limit_max = "max"
+    value = 10
+    rule.set_limit(limit_max, value)
+    assert set(rule.limits.keys()) == {limit_min, limit_max}
+
+
+def test_initial_limits():
+    lmin = "min"
+    lmax = "max"
+    limits = {
+        lmin: 0,
+        lmax: 10,
+    }
+
+    rule = Rules(limits)
+    assert len(rule.LIMITS) == 2
+    assert lmin in rule.LIMITS
+    assert lmax in rule.LIMITS
+
+    new_limit = "new"
+    rule.set_limit(new_limit, "Hello")
+    assert len(rule.LIMITS) == 2
+    assert lmin in rule.LIMITS
+    assert lmax in rule.LIMITS
+
+
+def test_make_preparations(rule):
+    rule.make_preparations()
+
+
+def test_make_turn(rule):
+    rule.make_turn()
