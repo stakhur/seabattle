5ece10fc95e63df9b854c57b64a759655ccb1f64
Jura Stachurski <pushthebo@gmail.com>
2024-10-25 18:16:00 +0200

    player alg added

--- seabattle/player.py
+++ seabattle/player.py
@@ -1,6 +1,12 @@
+from enum import Enum
+
 from .rules import Rules
 
 class Player:
+    
+    class State(Enum):
+        WAITING_FOR_RULES = 1
+        READY_TO_PREPARE = 2
     
     def __init__(self, name, ai=False):
         self._name = name
@@ -12,6 +18,13 @@ class Player:
     def name(self):
         return self._name
     
+    @property
+    def state(self):
+        _state = self.State.WAITING_FOR_RULES
+        if (self._rules != None):
+            _state = self.State.READY_TO_PREPARE
+
+        return _state
 
     def set_rules(self, rules: Rules):
         self._rules = rules
--- tests/test_player.py
+++ tests/test_player.py
@@ -11,17 +11,17 @@ class TestNewPlayer():
         assert player.name == name
         assert player._rules == None
         assert player._ai == False
+        assert player.state == Player.State.WAITING_FOR_RULES
 
     def test_new_player_ai(self):
         player = Player("AI", True)
         assert player._ai == True
-
+        assert player.state == Player.State.WAITING_FOR_RULES
 
 def test_set_rules():
     player = Player("Jura")
     player.set_rules(dict())
-    # assert player.
-
+    assert player.state == Player.State.READY_TO_PREPARE
 
 
 class TestPrepareToGame:
@@ -37,5 +37,5 @@ class TestPrepareToGame:
         return _player
 
 
-    # def test_prepare_to_game(self, human):
-        # human.prepare_to_game()
+    def test_prepare_to_game(self, human):
+        human.prepare_to_game()
